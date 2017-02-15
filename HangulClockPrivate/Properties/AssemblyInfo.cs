5abd7c71df014fcfaf3f4a21411f93f3c42f6af3
MR.LEE(LEE Sang Hun) <dsa28s@naver.com>
2017-02-15 19:53:36 +0900

    Add Private setting for hangul clock

---
+++ HangulClockPrivate/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
+// 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
+// 이러한 특성 값을 변경하세요.
+[assembly: AssemblyTitle("HangulClockPrivate")]
+[assembly: AssemblyDescription("Private setting engine for hangul clock.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MR.LEE(leeshoon1344)")]
+[assembly: AssemblyProduct("Hangul Clock Private Setting Engine")]
+[assembly: AssemblyCopyright("Copyright © 2017 MR.LEE(상훈). All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible을 false로 설정하면 이 어셈블리의 형식이 COM 구성 요소에 
+// 표시되지 않습니다.  COM에서 이 어셈블리의 형식에 액세스하려면 
+// 해당 형식에 대해 ComVisible 특성을 true로 설정하세요.
+[assembly: ComVisible(false)]
+
+// 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
+[assembly: Guid("d411d767-6c3a-48e9-b25c-e5001760cbcc")]
+
+// 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
+//
+//      주 버전
+//      부 버전 
+//      빌드 번호
+//      수정 버전
+//
+// 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 번호가 자동으로 
+// 지정되도록 할 수 있습니다.
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1")]
+[assembly: AssemblyFileVersion("1.1")]
---
+++ HangulClockPrivate/Settings.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HangulClockPrivate
+{
+    public static class Settings
+    {
+        public static String SERVICE_ENABLED = "a03cfb0f8a0e48cb848f7ba3287b3ecc";
+
+        public static String USE_WALLPAPER = "5fcd3ecae2624959b756fb902080c03d";
+        public static String USE_SOLID = "61407e0777724d04bd6b07004d520880";
+        public static String SOLID_COLOR = "28438bb86a804674be332816a441b3c2";
+
+        public static String NAME = "a35a895ade424abda1d1fcfffb819a7";
+        public static String THEME = "39a03ec41c5247a7a922ac2b6b2c6f53";
+
+        private static string SECTION = typeof(Settings).Namespace;
+        private static string settingsPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location) + @"\hangulClock.hcp";
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+        public static String GetString(String name)
+        {
+            StringBuilder temp = new StringBuilder(255);
+            int i = GetPrivateProfileString(SECTION, name, "", temp, 255, settingsPath);
+            return temp.ToString();
+        }
+        public static String Get(String name, String defVal)
+        {
+            return Get(SECTION, name, defVal);
+        }
+        public static String Get(string _SECTION, String name, String defVal)
+        {
+            StringBuilder temp = new StringBuilder(255);
+            int i = GetPrivateProfileString(_SECTION, name, "", temp, 255, settingsPath);
+            return temp.ToString();
+        }
+        public static Boolean Get(String name, Boolean defVal)
+        {
+            return Get(SECTION, name, defVal);
+        }
+        public static Boolean Get(string _SECTION, String name, Boolean defVal)
+        {
+            StringBuilder temp = new StringBuilder(255);
+            int i = GetPrivateProfileString(_SECTION, name, "", temp, 255, settingsPath);
+            bool retval = false;
+            if (bool.TryParse(temp.ToString(), out retval))
+            {
+                return retval;
+            }
+            else
+            {
+                return retval;
+            }
+        }
+        public static int Get(String name, int defVal)
+        {
+            return Get(SECTION, name, defVal);
+        }
+        public static int Get(string _SECTION, String name, int defVal)
+        {
+            StringBuilder temp = new StringBuilder(255);
+            int i = GetPrivateProfileString(SECTION, name, "", temp, 255, settingsPath);
+            int retval = 0;
+            if (int.TryParse(temp.ToString(), out retval))
+            {
+                return retval;
+            }
+            else
+            {
+                return retval;
+            }
+        }
+        public static void Set(String name, String val)
+        {
+            Set(SECTION, name, val);
+        }
+        public static void Set(string _SECTION, String name, String val)
+        {
+            WritePrivateProfileString(_SECTION, name, val, settingsPath);
+        }
+        public static void Set(String name, Boolean val)
+        {
+            Set(SECTION, name, val);
+        }
+        public static void Set(string _SECTION, String name, Boolean val)
+        {
+            WritePrivateProfileString(_SECTION, name, val.ToString(), settingsPath);
+        }
+        public static void Set(String name, int val)
+        {
+            Set(SECTION, name, val);
+        }
+        public static void Set(string _SECTION, String name, int val)
+        {
+            WritePrivateProfileString(SECTION, name, val.ToString(), settingsPath);
+        }
+    }
+}
