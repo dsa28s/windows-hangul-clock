bcd3e54bb5b1bb80da41dc4ea78cb6e8fde9a1c3
Lee Dora <dsa28s@naver.com>
2018-10-07 04:12:00 +0900

    Change monitoring service logic

--- HangulClockMonitoringService/Service1.cs
+++ HangulClockMonitoringService/Service1.cs
@@ -18,8 +18,8 @@ namespace HangulClockMonitoringService
 {
     public partial class Service1 : ServiceBase
     {
-        private Thread serviceThread;
-        private Thread explorerProcessThread;
+        private System.Timers.Timer serviceTimer;
+        private System.Timers.Timer explorerProcessTimer;
 
         private List<ScreenModel> screenModels = new List<ScreenModel>();
 
@@ -32,117 +32,143 @@ namespace HangulClockMonitoringService
         protected override void OnStart(string[] args)
         {
             LogKit.Info("Starting HangulClock Monitoring Service!");
+
+            serviceTimer = new System.Timers.Timer(3 * 1000);
+            serviceTimer.Elapsed += new System.Timers.ElapsedEventHandler(serviceTimer_Elapsed);
+            serviceTimer.Interval = 3000;
+            serviceTimer.Start();
+
+            explorerProcessTimer = new System.Timers.Timer(1 * 1000);
+            explorerProcessTimer.Elapsed += new System.Timers.ElapsedEventHandler(explorerProcessTimer_Elapsed);
+            explorerProcessTimer.Interval = 1000;
+            explorerProcessTimer.Start();
+
             System.Diagnostics.Debugger.Launch();
-
-            serviceThread = new Thread(ServiceThread);
-            explorerProcessThread = new Thread(ExplorerProcessCheckThread);
-
-            serviceThread.Start();
-            explorerProcessThread.Start();
         }
 
         protected override void OnStop()
         {
             LogKit.Info("HangulClock Monitoring Service Stopped.");
 
-            serviceThread.Interrupt();
-            explorerProcessThread.Interrupt();
+            serviceTimer.Stop();
+            explorerProcessTimer.Stop();
         }
 
-        private void ExplorerProcessCheckThread()
+        private void serviceTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
-            while (true)
-            {
-                try
-                {
-                    Process[] explorerProcesses = Process.GetProcessesByName("explorer");
+            var DataKit = new DataKit();
+            DataKit.Realm.Refresh();
 
-                    if (explorerProcesses.Length <= 0) // Windows 탐색기가 죽었으니까 한글시계도 실행될 필요는 없을듯...?
+            if (screenModels.Count <= 0)
+            {
+                foreach (var screen in System.Windows.Forms.Screen.AllScreens)
+                {
+                    screenModels.Add(MakeScreenModel(screen));
+                }
+            }
+
+            var screens = System.Windows.Forms.Screen.AllScreens;
+            int index = 0;
+
+            if (screens.Length != screenModels.Count) // 모니터의 개수가 줄었거나, 추가됬거나...
+            {
+                RestartHangulClockRenderer();
+                return;
+            }
+
+            // var clockSetting = DataKit.Realm.All<ClockSettingsByMonitor>().Where(c => c.MonitorDeviceName == screen.DeviceName).First();
+
+            bool isNotChanged = true;
+            foreach (var screen in screens)
+            {
+                if (MakeScreenModel(screen) != screenModels[index])
+                {
+                    isNotChanged = false;
+                    break;
+                }
+
+                index++;
+            }
+
+            if (!isNotChanged) // 음... 머 어떤 모니터의 해상도라던지 위치 등이 변경된듯...?
+            {
+                RestartHangulClockRenderer();
+                return;
+            }
+
+            Process[] hangulClockRendererProcesses = Process.GetProcessesByName("HangulClockRenderer");
+
+            if (hangulClockRendererProcesses.Length <= 0) // 서비스는 실행됐는데 한글시계 프로세스가 1도 없으면...
+            {
+                RestartHangulClockRenderer(); // 한글시계 프로세스 시작
+            }
+            else // 근데... 머 하나는 실행되있고, 하나는 실행이 안되어있으면 이건 검사해야해
+            {
+                foreach (var item in System.Windows.Forms.Screen.AllScreens.Select((value, i) => new { i, value }))
+                {
+                    var clockSetting = DataKit.Realm.All<ClockSettingsByMonitor>().Where(c => c.MonitorDeviceName == item.value.DeviceName).First();
+
+                    if (clockSetting.isUseHangulClock) // 해당 모니터에서 한글시계를 사용한다네...?
                     {
-                        Process[] hangulClockRendererProcesses = Process.GetProcessesByName("HangulClockRenderer");
+                        var isRunning = false;
 
                         foreach (var hangulClockRendererProcess in hangulClockRendererProcesses)
                         {
-                            hangulClockRendererProcess.Kill();
+                            if (hangulClockRendererProcess.StartInfo.Arguments == String.Format("/mindex {0}", item.i))
+                            {
+                                isRunning = true;
+                            }
                         }
-                    }
-                    else // 다시 나왔으니 실행하자
-                    {
-                        Process[] hangulClockRendererProcesses = Process.GetProcessesByName("HangulClockRenderer");
 
-                        if (hangulClockRendererProcesses.Length <= 0)
+                        if (!isRunning)
                         {
-                            RestartHangulClockRenderer();
+                            Process hangulClockRendererProcess = new Process();
+                            hangulClockRendererProcess.StartInfo = new ProcessStartInfo("HangulClockRenderer.exe");
+                            hangulClockRendererProcess.StartInfo.WorkingDirectory = @"C:\Program Files\Hangul Clock";
+                            hangulClockRendererProcess.StartInfo.Arguments = String.Format("/mindex {0}", item.i);
+                            hangulClockRendererProcess.StartInfo.CreateNoWindow = true;
+                            hangulClockRendererProcess.StartInfo.UseShellExecute = false;
+
+                            hangulClockRendererProcess.Start();
                         }
                     }
-
-                    Thread.Sleep(1000);
-                }
-                catch (Exception e)
-                {
-
                 }
             }
         }
 
-        private void ServiceThread()
+        private void explorerProcessTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
-            var DataKit = new DataKit();
+            Process[] explorerProcesses = Process.GetProcessesByName("explorer");
 
-            while (true)
+            if (explorerProcesses.Length <= 0) // Windows 탐색기가 죽었으니까 한글시계도 실행될 필요는 없을듯...?
             {
-                DataKit.Realm.Refresh();
+                Process[] hangulClockRendererProcesses = Process.GetProcessesByName("HangulClockRenderer");
 
-                try
+                foreach (var hangulClockRendererProcess in hangulClockRendererProcesses)
                 {
-                    Thread.Sleep(3000);
-
-                    if (screenModels.Count <= 0)
-                    {
-                        foreach (var screen in System.Windows.Forms.Screen.AllScreens)
-                        {
-                            screenModels.Add(MakeScreenModel(screen));
-                        }
-                    }
-
-                    var screens = System.Windows.Forms.Screen.AllScreens;
-                    int index = 0;
-
-                    if (screens.Length != screenModels.Count) // 모니터의 개수가 줄었거나, 추가됬거나...
-                    {
-                        RestartHangulClockRenderer();
-                        return;
-                    }
-
-                    // var clockSetting = DataKit.Realm.All<ClockSettingsByMonitor>().Where(c => c.MonitorDeviceName == screen.DeviceName).First();
-
-                    bool isNotChanged = true;
-                    foreach (var screen in screens)
-                    {
-                        if (MakeScreenModel(screen) != screenModels[index])
-                        {
-                            isNotChanged = false;
-                            break;
-                        }
-
-                        index++;
-                    }
-
-                    if (!isNotChanged) // 음... 머 어떤 모니터의 해상도라던지 위치 등이 변경된듯...?
-                    {
-                        RestartHangulClockRenderer();
-                        return;
-                    }
+                    hangulClockRendererProcess.Kill();
                 }
-                catch (Exception e)
-                {
+            }
+            else // 다시 나왔으니 실행하자
+            {
+                Process[] hangulClockRendererProcesses = Process.GetProcessesByName("HangulClockRenderer");
 
+                if (hangulClockRendererProcesses.Length <= 0)
+                {
+                    RestartHangulClockRenderer();
                 }
             }
         }
 
         private void RestartHangulClockRenderer()
         {
+            Process[] hangulClockRendererProcesses = Process.GetProcessesByName("HangulClockRenderer");
+
+            foreach (var hangulClockRendererProcess in hangulClockRendererProcesses)
+            {
+                hangulClockRendererProcess.Kill();
+            }
+
             var DataKit = new DataKit();
 
             foreach (var item in System.Windows.Forms.Screen.AllScreens.Select((value, i) => new { i, value }))
